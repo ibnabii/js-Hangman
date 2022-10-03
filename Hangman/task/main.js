24a60cdf5ee82326ad6166dac6708f32f687d086
ibnabii <someone@somewhere.net>
2022-10-03 22:07:59 +0200

    Stage 7

--- Hangman/task/main.js
+++ Hangman/task/main.js
@@ -14,30 +14,60 @@ function randomWord (array) {
     return array[Math.floor(Math.random() * array.length)];
 }
 
+
+function goodChoice (letter) {
+    if (letter === null ) {
+        return false;
+    }
+    if (letter.length !== 1) {
+        console.log('Please, input a single letter.');
+        return false;
+    }
+
+    if (tried.includes(letter)) {
+        console.log('You\'ve already guessed this letter.');
+        return false;
+    }
+
+    if (letter.match(/[a-z]/)) {
+        return true;
+    } else {
+        console.log('Please, enter a lowercase letter from the English alphabet.');
+        return false;
+    }
+}
+
+
+function getLetter () {
+    let letter = null;
+    while (!goodChoice(letter))
+    {
+        console.log(`\n${current.join('')}`);
+        letter = input('Input a letter: ');
+    }
+    return letter;
+}
+
 let attempts = 8;
 printWelcome();
 
 let target = randomWord(words);
 let current = '-'.repeat(target.length).split('');
+let tried = []
 
 while (attempts > 0) {
-    console.log(`\n${current.join('')}`);
-    let letter = input('Input a letter: ');
+    let letter = getLetter();
+    tried.push(letter);
     if (target.includes(letter)) { // good letter
-        if (current.includes(letter)) { // letter already guessed
-            console.log('No improvements.');
-            attempts--;
-        } else { // new letter
-            for (let l = 0; l < target.length; l++) {
-                if (target.charAt(l) === letter) {
-                    current[l] = letter;
-                }
+        for (let l = 0; l < target.length; l++) {
+            if (target.charAt(l) === letter) {
+                current[l] = letter;
             }
-            // check if won
-            if (!current.includes('-')) {
-                console.log('You guessed the word!\nYou survived!');
-                attempts = -1;
-            }
+        }
+        // check if won
+        if (!current.includes('-')) {
+            console.log(`You guessed the word ${target}!\nYou survived!`);
+            attempts = -1;
         }
     } else {
         console.log("That letter doesn't appear in the word.")
