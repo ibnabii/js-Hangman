0cdcd51655e063c65970632497d93c8f42cfdee2
ibnabii <someone@somewhere.net>
2022-10-03 21:49:03 +0200

    Stage 6

--- Hangman/task/main.js
+++ Hangman/task/main.js
@@ -14,32 +14,37 @@ function randomWord (array) {
     return array[Math.floor(Math.random() * array.length)];
 }
 
-const attempts = 8;
+let attempts = 8;
 printWelcome();
 
 let target = randomWord(words);
 let current = '-'.repeat(target.length).split('');
 
-for (let i = 1; i <= attempts; i++) {
+while (attempts > 0) {
     console.log(`\n${current.join('')}`);
     let letter = input('Input a letter: ');
-    if (target.includes(letter)) {
-        for (let l = 0; l < target.length; l++) {
-            if (target.charAt(l) === letter) {
-                current[l] = letter;
+    if (target.includes(letter)) { // good letter
+        if (current.includes(letter)) { // letter already guessed
+            console.log('No improvements.');
+            attempts--;
+        } else { // new letter
+            for (let l = 0; l < target.length; l++) {
+                if (target.charAt(l) === letter) {
+                    current[l] = letter;
+                }
+            }
+            // check if won
+            if (!current.includes('-')) {
+                console.log('You guessed the word!\nYou survived!');
+                attempts = -1;
             }
         }
     } else {
         console.log("That letter doesn't appear in the word.")
+        attempts--;
     }
 }
-console.log('Thanks for playing!');
-
-/*
-let word = input(`Guess the word ${current.join('')}:`);
-if (word === target) {
-    console.log('You survived!');
-} else {
-    console.log('You lost!');
-}*/
-
+if (attempts === 0) {
+    console.log('\nYou lost!')
+}
+//console.log('Thanks for playing!');
