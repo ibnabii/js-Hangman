9c4ab789251b8d2cf1cf21b47cc9601f7b1ba96d
ibnabii <someone@somewhere.net>
2022-10-03 17:46:48 +0200

    Initial commit after Stage 1

---
+++ Hangman/task/main.js
@@ -0,0 +1,12 @@
+// Use "input()" to input a line from the user
+// Use "input(str)" to print some text before requesting input
+// You will need this in the following stages
+const input = require('sync-input')
+const Console = require("console");
+
+function printWelcome () {
+    console.log("H A N G M A N")
+    console.log("The game will be available soon.")
+}
+
+printWelcome();
