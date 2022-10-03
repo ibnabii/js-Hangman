7af84638655e0ae1646e1352a8e4d54bf9ef7bc2
ibnabii <someone@somewhere.net>
2022-10-03 22:23:27 +0200

    Stage 8 - final

--- Hangman/task/main.js
+++ Hangman/task/main.js
@@ -48,33 +48,54 @@ function getLetter () {
     return letter;
 }
 
-let attempts = 8;
-printWelcome();
+function play () {
+    while (attempts > 0) {
+        let letter = getLetter();
+        tried.push(letter);
+        if (target.includes(letter)) { // good letter
+            for (let l = 0; l < target.length; l++) {
+                if (target.charAt(l) === letter) {
+                    current[l] = letter;
+                }
+            }
+            // check if won
+            if (!current.includes('-')) {
+                console.log(`You guessed the word ${target}!\nYou survived!`);
+                wins++;
+                return 1;
+            }
+        } else {
+            console.log("That letter doesn't appear in the word.")
+            attempts--;
+        }
+    }
+    console.log('\nYou lost!')
+    loses++;
+    return 0;
+}
 
+
+printWelcome();
+let wins = 0;
+let loses = 0;
+let attempts = 8;
 let target = randomWord(words);
 let current = '-'.repeat(target.length).split('');
-let tried = []
+let tried = [];
 
-while (attempts > 0) {
-    let letter = getLetter();
-    tried.push(letter);
-    if (target.includes(letter)) { // good letter
-        for (let l = 0; l < target.length; l++) {
-            if (target.charAt(l) === letter) {
-                current[l] = letter;
-            }
-        }
-        // check if won
-        if (!current.includes('-')) {
-            console.log(`You guessed the word ${target}!\nYou survived!`);
-            attempts = -1;
-        }
-    } else {
-        console.log("That letter doesn't appear in the word.")
-        attempts--;
+let cmd = input('Type "play" to play the game, "results" to show the scoreboard, and "exit" to quit:')
+while (cmd !== 'exit') {
+    if (cmd === 'play') {
+        attempts = 8;
+        target = randomWord(words);
+        current = '-'.repeat(target.length).split('');
+        tried = [];
+        play();
     }
+    if (cmd === 'results') {
+        console.log(`You won: ${wins} times.\nYou lost: ${loses} times.`);
+    }
+    cmd = input('Type "play" to play the game, "results" to show the scoreboard, and "exit" to quit:')
 }
-if (attempts === 0) {
-    console.log('\nYou lost!')
-}
-//console.log('Thanks for playing!');
+
+
